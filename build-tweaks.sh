a15591cadd6225c96676d174da928366b101855c
Judd Storrs <jstorrs@gmail.com>
2025-04-11 14:05:29 -0400

    Bugfix

--- build-tweaks.sh
+++ build-tweaks.sh
@@ -10,25 +10,27 @@ export DCMTK_ENABLE_PRIVATE_TAGS=ON
 export DCMTK_PORTABLE_LINUX_BINARIES=ON
 
 ROOT=$(dirname "$(readlink -e "$0")")
+BUILD_DIR=$ROOT/build
+
 case $(basename $0 .sh) in
     configure)
-	cd $ROOT
+	cd $BUILD_DIR
 	[ -f CMakeLists.txt ] || cmake ..
     ;;
 
     build)
-	cd $ROOT/tweaks
+	cd $BUILD_DIR/tweaks
 	make -j$(nproc) all
     ;;
 
     install)
-	cd $ROOT/tweaks
+	cd $BUILD_DIR/tweaks
 	sudo cmake -DCMAKE_INSTALL_COMPONENT=bin -P cmake_install.cmake
     ;;
 
     *)
-	mkdir -p $ROOT/build
-	cd $ROOT/build
+	mkdir -p $BUILD_DIR
+	cd $BUILD_DIR
 	ln -fs ../$(basename $0) configure.sh
 	ln -fs ../$(basename $0) build.sh
 	ln -fs ../$(basename $0) install.sh
